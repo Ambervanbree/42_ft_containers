ad439ac88eeb100a3a07e05044c3c038f160f59f
Amber <78368988+Ambervanbree@users.noreply.github.com>
2022-07-04 12:44:54 +0200

    Delete build directory

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /sgoinfre/goinfre/Perso/avan-bre/Projects/Circle04/ft_containers/googletest
-# Build directory: /sgoinfre/goinfre/Perso/avan-bre/Projects/Circle04/ft_containers/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
