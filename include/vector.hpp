690a5d466e63c7267776f3d7d118ce5b02e3bc39
Amber <78368988+Ambervanbree@users.noreply.github.com>
2022-07-04 17:51:50 +0200

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -10,83 +10,104 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_HPP
-# define VECTOR_HPP
+#ifndef VECTOR_TEST_HPP
+# define VECTOR_TEST_HPP
 
-namespace ft{
-	template <class T, class Allocator = allocator<T> >
-	class vector {
+namespace ft {
+	template <class T, class Allocator = std::allocator<T>>
+	class vector_test{
+		private:
+			T*			_array;
+			size_t		_size;
+			size_t		_capacity;
+			Allocator	_alloc;
+
+			void reallocate(){
+				Allocator	temp;
+				
+				this->_capacity *= 2;
+				this->_array = temp.allocate(this->_capacity);
+				this->_alloc.deallocate(0, this->_size);
+				this->_alloc = temp;
+			}
+
 		public:
 			// types:
-			typedef typename Allocator::reference reference;
-			typedef typename Allocator::const_reference const_reference;
-			typedef implementation defined iterator; // See 23.1
-			typedef implementation defined const_iterator; // See 23.1
-			typedef implementation defined size_type; // See 23.1
-			typedef implementation defined difference_type;// See 23.1
-			typedef T value_type;
-			typedef Allocator allocator_type;
-			typedef typename Allocator::pointer pointer;
-			typedef typename Allocator::const_pointer const_pointer
-			typedef std::reverse_iterator<iterator> reverse_iterator;
-			typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+			typedef typename Allocator::reference 			reference;
+			typedef typename Allocator::const_reference 	const_reference;
+			// typedef implementation defined iterator; // See 23.1
+			// typedef implementation defined const_iterator; // See 23.1
+			typedef size_t									size_type;
+			typedef size_t									difference_type;
+			typedef T 										value_type;
+			typedef Allocator								allocator_type;
+			typedef typename Allocator::pointer 			pointer;
+			typedef typename Allocator::const_pointer 		const_pointer;
+			// typedef std::reverse_iterator<iterator> 		reverse_iterator;
+			// typedef std::reverse_iterator<const_iterator> 	const_reverse_iterator;
+			
+			// construct/copy/destroy:
+			explicit vector_test(const allocator_type& alloc= allocator_type()) : 
+			_size(0), _capacity(0), _alloc(alloc) {
+				this->_array = this->_alloc.allocate(this->_capacity);
+			}
 
-			// 23.2.4.1 construct/copy/destroy:
-			explicit vector(const Allocator& = Allocator());
-			explicit vector(size_type n, const T& value = T(),
-			const Allocator& = Allocator());
-			template <class InputIterator>
-			vector(InputIterator first, InputIterator last,
-			const Allocator& = Allocator());
-			vector(const vector<T,Allocator>& x);
-			~vector();
-			vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
-			template <class InputIterator>
-			void assign(InputIterator first, InputIterator last);
-			void assign(size_type n, const T& u);
-			allocator_type get_allocator() const;
+			explicit vector_test(size_type n, const T& value = T(), const allocator_type& alloc= allocator_type()) :
+			_size(n), _capacity(n), _alloc(alloc) {
+				this->_array = this->_alloc.allocate(this->_capacity);
+				for (size_type i = 0; i < n; i++){
+					_array[i] = value;
+				}
+			}
 
-			// iterators:
-			iterator begin();
-			const_iterator begin() const;
-			iterator end();
-			const_iterator end() const;
-			reverse_iterator rbegin();
-			const_reverse_iterator rbegin() const;
-			reverse_iterator rend();
-			const_reverse_iterator rend() const;
+			~vector_test(){
+				this->_alloc.deallocate(this->_array, this->_capacity);
+			}
 
-			// 23.2.4.2 capacity:
-			size_type size() const;
-			size_type max_size() const;
-			void resize(size_type sz, T c = T());
-			size_type capacity() const;
-			bool empty() const;
-			void reserve(size_type n);
-	
+			// capacity:
+			size_type size() const {return this->_size; }
+			size_type capacity() const {return this->_capacity; }
+
 			// element access:
-			reference operator[](size_type n);
-			const_reference operator[](size_type n) const;
-			const_reference at(size_type n) const;
-			reference at(size_type n);
-			reference front();
-			const_reference front() const;
-			reference back();
-			const_reference back() const;
-	
-			// 23.2.4.3 modifiers:
-			void push_back(const T& x);
-			void pop_back();
-			iterator insert(iterator position, const T& x);
-			void insert(iterator position, size_type n, const T& x);
-			template <class InputIterator>
-			void insert(iterator position,
-			InputIterator first, InputIterator last);
-			iterator erase(iterator position);
-			iterator erase(iterator first, iterator last);
-			void swap(vector<T,Allocator>&);
-			void clear();
+			reference operator[](size_type n) {return this->_array[n]; }
+			const_reference operator[](size_type n) const {return this->_array[n]; }
+
+			// modifiers:
+			void push_back(const T& x){
+				if (this->_capacity == this->_size){
+					this->reallocate();
+				}
+				this->_array[this->_size] = x;
+				this->_size++;
+			}
+
+			// non-member operators:
+			friend bool operator==(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array == y._array ;
+			}
+
+			friend bool operator!=(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array != y._array ;
+			}
+
+			friend bool operator<(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array < y._array ;
+			}
+
+			friend bool operator>(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array > y._array ;
+			}
+
+			friend bool operator<=(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array <= y._array ;
+			}
+
+			friend bool operator>=(const ft::vector_test<T, Allocator>& x, const ft::vector_test<T, Allocator>& y){
+				return x._array >= y._array ;
+			}
 	};
 }
+
+#endif
 
 #endif
