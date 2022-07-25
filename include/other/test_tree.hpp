3db9205aab086716bff525d27c3bb5dcd939acf7
avan-bre <ambervanbree@gmail.com>
2022-07-25 21:48:18 +0200

    find parent

--- include/other/test_tree.hpp
+++ include/other/test_tree.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_tree.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amber <amber@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 10:57:18 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/07/21 19:08:16 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/07/22 12:48:12 by amber            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <iostream>
 # include <memory>
 # include <cassert>
+# include "pair.hpp"
 
 # define LEFT  0
 # define RIGHT 1
@@ -26,16 +27,17 @@
 namespace ft{
 	enum	color_t {RED, BLACK};
 	
+	template <class key, class value, class Allocator = std::allocator<ft::pair<key, value> > >
 	struct node{
-			// typedef	*node		node_ptr;
+			typedef	node *		node_ptr;
 
-			node		*_parent;
-			node		*_child[2];
-			color_t		_color;
-			int			_n;
+			node_ptr				_parent;
+			node_ptr				_child[2];
+			color_t					_color;
+			int						 _n;
+			ft::pair<key, value>	_content;
 
 			node(int n) : _n(n){
-				std::cout << "Node initialised with n: " << _n << std::endl;
 				_parent = NULL;
 				_left = NULL;
 				_right = NULL;
@@ -49,115 +51,96 @@ namespace ft{
 				_right = x._right;
 				return *this;
 			}
-
-			// node *parent() {return _parent; }
-			// node *child_left() {return _left; }
-			// node *child_right() {return _right; }
-			// color_t aunt_color() {
-			// 	if (_parent->_child[LEFT] == NULL || _parent->_child[RIGHT] == NULL)
-			// 		return BLACK;
-			// 	if (_n == _parent->_child[LEFT]->_n)
-			// 		return _parent->_child[RIGHT]->_color;
-			// 	else
-			// 		return _parent->_child[LEFT]->_color;
-			// }
-
-			// std::string color() {return _color ? "RED" : "BLACK"; }
-			// int n() {return _n; }
-			// void change_color(color_t color) {
-			// 	_color = color; 
-			// 	std::cout << "Color node with n " << _n << " changed to " << this->color() << std::endl;
-			// 	return; 
-			// }
-			
-			// void set_child(node *new_node, int dir) {
-			// 	_child[dir] = new_node; 
-			// 	std::cout << new_node->color() << " node with n " << new_node->n() << " is now child " 
-			// 	<< dir << " of " << color() << " parent with n " << _n << std::endl;
-			// 	return; }
-				
-			// void set_parent(node *parent){
-			// 	_parent = parent; 
-			// 	std::cout << "Node with n " << n() << " now has parent with n " 
-			// 	<< parent->_n << std::endl;
-			// 	return; }
 	};
 
-	node *create_node(int n){
+	template <class key, class value>
+	node<key, value> * create_node(int n){
 		node *new_node	= new node(n);
 		return new_node;
 	}
 
 	class tree{
-		private:
-			node	*_root;
-
 		public:
-			tree() {_root = NULL; }
-
-		node *rotate_dir(node *parent, int dir){
-			node	*grandma = parent->_parent;
-			node	*daughter = parent->_child[1 - dir];
-			node	*temp;
-
-			assert(daughter != NULL);
-			std::cout << " test test " << std::endl;
-			temp = daughter->_child[dir];
-			parent->_child[1 - dir] = temp;
-			if (temp != NULL) {temp->_parent = parent;}
-			daughter->_child[dir] = parent;
-			std::cout << "daughter is not null " << daughter->_n << std::endl;
-			parent->_parent = daughter;
-			daughter->_parent = grandma;
-			if (grandma != NULL)
-				grandma->_child[parent == grandma->_right ? RIGHT : LEFT] = daughter;
-			else
-				_root = daughter;
-			return daughter;
-		}
+			typedef ft::node *		node_ptr;
 			
-		void insert_node(node *current, node *parent, int dir){
+			tree() {_root = NULL; }
+			node_ptr get_root() {return _root; }
+
+			node_ptr find_parent(node_ptr new_node){
+				node_ptr	current = _root;
+				
+				while (1){
+					node_ptr next = current->_child[current->_n < new_node->_n];
+					if (next == NULL)
+						return current;
+					else
+						current = next;
+				}
+			}
+
+			node_ptr rotate_dir(node_ptr parent, int dir){
+				node_ptr	grandma = parent->_parent;
+				node_ptr	daughter = parent->_child[1 - dir];
+				node_ptr	temp;
+
+				assert(daughter != NULL);
+				temp = daughter->_child[dir];
+				parent->_child[1 - dir] = temp;
+				if (temp != NULL) {temp->_parent = parent;}
+				daughter->_child[dir] = parent;
+				parent->_parent = daughter;
+				daughter->_parent = grandma;
+				if (grandma != NULL)
+					grandma->_child[parent == grandma->_right ? RIGHT : LEFT] = daughter;
+				else
+					_root = daughter;
+				return daughter;
+			}
+			
+		void insert_node(node_ptr current, node_ptr parent, int dir){
 			current->_parent = parent;
-			// case 1
 			if (_root == NULL) {
 				_root = current;
 				std::cout << "Node with n " << current->_n << " is now root " << std::endl;
 				return ;
 				}
 			parent->_child[dir] = current;
-			node	*grandma = parent->_parent;
-			
-			// case 4 (parent is now red)
-			if (grandma == NULL){
-				parent->_color = BLACK;
-				return;
-			}
-			dir = childDir(parent);
-			node	*aunt = grandma->_child[1 - dir];
-			
-			// case 5 + 6 (grandma exists)
-			if (aunt == NULL || aunt->_color == BLACK){
-				std::cout << "in aunt black " << current->_n << " dir " << dir << std::endl;
-				// if the node is the inner grand child we put it between parent and grandparent
-				// before we do the color flip
-				if (current == parent->_child[1 - dir]){
-					rotate_dir(parent, dir);
-					current = parent;
-					parent = grandma->_child[dir];
+			do{	
+				// case 1
+				if (parent->_color == BLACK) {return; }	
+				node_ptr	grandma = parent->_parent;
+				// case 4 (parent is now red)
+				if (grandma == NULL){
+					parent->_color = BLACK;
+					return;
 				}
-				rotate_dir(grandma, 1 - dir);
+				dir = childDir(parent);
+				node_ptr	aunt = grandma->_child[1 - dir];
+				
+				// case 5 + 6 (grandma exists)
+				if (aunt == NULL || aunt->_color == BLACK){
+					std::cout << "in aunt black " << current->_n << " dir " << dir << std::endl;
+					// if the node is the inner grand child we put it between parent and grandparent
+					// before we do the color flip
+					if (current == parent->_child[1 - dir]){
+						rotate_dir(parent, dir);
+						current = parent;
+						parent = grandma->_child[dir];
+					}
+					rotate_dir(grandma, 1 - dir);
+					parent->_color = BLACK;
+					grandma->_color = RED;
+					return;
+				}
+				// case 2, aunt, parent and grandma can be repainted
 				parent->_color = BLACK;
+				aunt->_color = BLACK;
 				grandma->_color = RED;
-				return;
-			}
-			// case 2, aunt, parent and grandma can be repainted
-			parent->_color = BLACK;
-			aunt->_color = BLACK;
-			grandma->_color = RED;
-			current = grandma;
+				current = grandma;
+			}while ((parent = current->_parent) != NULL);
 		}
 
-		void visualise(node *node, std::string indent, bool last){
+		void visualise(node_ptr node, std::string indent, bool last){
 			if (node != NULL){
 				std::cout << indent;
 				if (last){
@@ -174,34 +157,10 @@ namespace ft{
 			}
 			
 		}
+
+		private:
+			node_ptr	_root;
 	};
 }
 
-
-
 #endif
-
-
-	// // Print tree
-	// void    _recursive_print(node_ptr node, std::string indent, bool last) const
-	// {
-	// 	if (!is_nil(node))
-	// 	{
-	// 		std::cout << indent;
-	// 		if (last)
-	// 		{
-	// 			std::cout << "R----";
-	// 			indent += "     ";
-	// 		}
-	// 		else
-	// 		{
-	// 			std::cout << "L----";
-	// 			indent += "|    ";
-	// 		}
-	// 		std::string sColor = node->color ? "BLACK" : "RED";
-	// 		std::cout << node->value << "(" << sColor << ")" 
-	// 					<< std::endl;
-	// 		this->_recursive_print(node->child[LEFT], indent, false);
-	// 		this->_recursive_print(node->child[RIGHT], indent, true);
-	// 	}
-	// }
--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amber <amber@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 11:50:55 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/07/21 18:53:09 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/07/22 11:24:01 by amber            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,26 @@ int		main(void){
 	ft::node	*node3 = ft::create_node(4);
 	ft::node	*node4 = ft::create_node(1);
 	ft::node	*node5 = ft::create_node(8);
-	// ft::node	*node6 = ft::create_node(1);
-	// ft::node	*node7 = ft::create_node(1);
-	// ft::node	*node8 = ft::create_node(1);
+	ft::node	*node6 = ft::create_node(3);
+	ft::node	*node7 = ft::create_node(10);
+	ft::node	*node8 = ft::create_node(15);
+	ft::node	*node9 = ft::create_node(16);
+	ft::node	*node10 = ft::create_node(17);
 
 	
 	tree.insert_node(node, NULL, 0);	
 	tree.insert_node(node2, node, node->_n < node2->_n);
 	tree.insert_node(node3, node, node->_n < node3->_n);
 	tree.insert_node(node4, node3, node3->_n < node4->_n);
-	tree.visualise(node, "", false);
 	tree.insert_node(node5, node2, node2->_n < node5->_n);
+	tree.insert_node(node6, node4, node4->_n < node6->_n);
+	tree.insert_node(node7, node5, node5->_n < node7->_n);
+	tree.insert_node(node8, node7, node7->_n < node8->_n);
+	tree.insert_node(node9, node8, node8->_n < node9->_n);
+	std::cout << "Parent is " << tree.find_parent(node10)->_n << std::endl;
+	// here it goes wrong
+
+	// tree.insert_node(node10, node9, node9->_n < node10->_n);
+
 	tree.visualise(node, "", false);
 }
